fbb1da1f137e41a8d84fd8fe76c7a6bab9967036
weagan22 <weagan22@gmail.com>
2019-02-17 19:06:13 -0800

    Added date functionality.

--- FileRenamer.vb
+++ FileRenamer.vb
@@ -1,19 +1,29 @@
 ﻿Module Module1
 
     Sub Main()
+
         Dim Arguments() As String = Environment.GetCommandLineArgs()
         If Arguments.Length > 1 Then
 
             Dim inFilePath As String = Arguments(1)
-            MsgBox(inFilePath)
-            'Dim extension As String = System.IO.Path.GetExtension(inFilePath)
-            'Dim fileName As String = System.IO.Path.GetFileName(inFilePath)
 
-            'fileName = Left(fileName, InStrRev(fileName, ".") - 1)
-            'fileName = Replace(fileName, ".", " ")
-            'fileName = fileName & extension
+            If Arguments.Length > 2 Then
+                For i = 2 To UBound(Arguments)
+                    inFilePath = inFilePath & " " & Arguments(i)
+                Next
+            End If
 
-            'My.Computer.FileSystem.RenameFile(inFilePath, fileName)
+            Dim fileExtension As String = System.IO.Path.GetExtension(inFilePath)
+            Dim fileName As String = System.IO.Path.GetFileNameWithoutExtension(inFilePath)
+
+            fileName = Replace(fileName, ".", " ")
+            If Text.RegularExpressions.Regex.IsMatch(fileName, "(19|20)[0-9][0-9]") Then
+                fileName = Left(fileName, InStr(fileName, Text.RegularExpressions.Regex.Match(fileName, "(19|20)[0-9][0-9]").ToString) - 2)
+            End If
+
+            fileName = fileName & fileExtension
+
+            My.Computer.FileSystem.RenameFile(inFilePath, fileName)
         End If
     End Sub
 
